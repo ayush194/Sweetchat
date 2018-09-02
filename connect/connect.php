a320c852b0d080e402d9e4b14efb03272612e2cb
Ayush Kumar <kr.ayush998@gmail.com>
2018-09-03 03:18:05 +0530

    Edited connect.php for establishing Cleardb connection

--- connect/connect.php
+++ connect/connect.php
@@ -1,12 +1,21 @@
 <?php
 function connect() {
-	//$conn = new mysqli("localhost", "root", "crap-bag", "sweetchat");
-	//For deploying on heroku
-	$conn = new mysqli("us-cdbr-iron-east-01.cleardb.net", "bb6c241fee9ae2", "15d3b257", "heroku_ffb51ce9427a989");
+	/*
+	$conn = new mysqli("localhost", "root", "crap-bag", "sweetchat");
   //check for connection error
   if ($conn->connect_error) {
     die("Cannot connect to database : " . $conn->connect_error);
   }
+	*/
+  //For deploying on heroku
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$conn = new mysqli($server, $username, $password, $db);
   return $conn;
 }
 ?>

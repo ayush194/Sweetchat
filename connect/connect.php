012e43847b65840102d65a7fe5ac93de58e96c30
Ayush Kumar <kr.ayush998@gmail.com>
2018-09-04 05:00:33 +0530

    reorganized again in deploy branch

--- connect/connect.php
+++ connect/connect.php
@@ -1,7 +1,7 @@
 <?php
 function connect() {
 	/*
-	$conn = new mysqli("localhost", "root", "crap-bag", "sweetchat");
+	$conn = new mysqli("localhost", "root", "starfish12345", "sweetchat");
   //check for connection error
   if ($conn->connect_error) {
     die("Cannot connect to database : " . $conn->connect_error);

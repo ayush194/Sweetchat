f8e940660a21683a661e48a63a0372e02722b992
Ayush Kumar <kr.ayush998@gmail.com>
2019-09-24 01:40:44 +0530

    Modified sweetchat_db SQL

--- sweetchat_db_5.7.sql
+++ sweetchat_db_5.7.sql
@@ -50,12 +50,22 @@ create table chats (
 	primary key(chatid)
 );
 
-alter table relationships add foreign key relationships_statusid_fk (statusid) references status(statusid);
-alter table messages add foreign key messages_chatid_fk (chatid) references chats(chatid);
-alter table messages add foreign key messages_userid_fk (userid) references users(userid);
-alter table chatusers add foreign key chatusers_userid_fk (userid) references users(userid);
-alter table chatusers add foreign key chatusers_chatid_fk (chatid) references chats(chatid);
-alter table chats add foreign key chats_userid_fk (userid) references users(userid);
+alter table relationships add foreign key relationships_ibfk_1 (statusid) references status(statusid) on delete cascade;
+alter table messages add foreign key messages_ibfk_1 (chatid) references chats(chatid) on delete cascade;
+alter table messages add foreign key messages_ibfk_2 (userid) references users(userid) on delete cascade;
+alter table chatusers add foreign key chatusers_ibfk_1 (userid) references users(userid) on delete cascade;
+alter table chatusers add foreign key chatusers_ibfk_2 (chatid) references chats(chatid) on delete cascade;
+alter table chats add foreign key chats_ibfk_1 (userid) references users(userid) on delete cascade;
 
+/*
+foreign keys are added to establish parent-child relationships
+for example in the above tables, the 'users' table is the parent while chatusers is a child
+(similarly 'chats' table is a parent while 'chatusers' is its child)
+because an entry in the child points/links/references to a specific entry in the parent. Now if this 
+entry in the child is deleted, the corresponding parent entry will not be affected but if
+the parent entry is deleted the linked child entries must be deleted (obviously it will make no sense
+to store the chats of a user who has been deleted) and it will be done so if the 'on delete cascade'
+attribute was provided, if it was not provided then this will raise an error.
+*/
 
 
